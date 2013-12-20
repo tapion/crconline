012550e60b903a7def100a6b41a9fcceab3aea7f
jerson <jerson@localhost.localdomain>
2013-12-20 16:55:41 -0500

    se integra parsley con los formularios, de login y administracion de servicios

--- application/models/usuario.php
+++ application/models/usuario.php
@@ -21,7 +21,7 @@ class Usuario extends CI_Model {
          * @param string $password Contraseña
          */
 	public function autenticar( $login='', $password='' ) {
-            if( !empty($login) && !empty($password)){                
+            if(!empty($login) && !empty($password)){                
                 $this->db->select('*');
                 $this->db->from('usuarios');
                 $this->db->where('usuario_login', $login);

4757c25ff14524fc0ab45badace99398a5b0f79c
jerson <jerson@localhost.localdomain>
2013-12-26 11:07:38 -0500

    Se corrige el response de la tabla consultar listados

--- application/views/adminServicio/consultar.php
+++ application/views/adminServicio/consultar.php
@@ -10,52 +10,58 @@ if (isset($exito) && !empty($exito)) {
     <?php
 }
 ?>    
-<div class="table-responsive">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="text-muted bootstrap-admin-box-title">Listado De Servicios</div>
-        </div>
-        <div class="bootstrap-admin-panel-content">
+<table>
+    <tr>
+        <td>
+            <div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="text-muted bootstrap-admin-box-title">Listado De Servicios</div>
+                    </div>
+                    <div class="bootstrap-admin-panel-content">
 
-            <table cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Sede</th>
-                        <th>Servicio</th>
-                        <th>Vlr Servicio</th>
-                        <th>Vlr Certificado</th>
-                        <th>Tipo Servicio</th>
-                        <th>Estado</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    foreach ($filtros as $dtsFiltros) {
-                        ?>
-                        <tr>
-                            <td><?= $dtsFiltros->sede_nombre; ?></td>
-                            <td><?= $dtsFiltros->servicio_nombre; ?></td>
-                            <td><?= $dtsFiltros->servicio_valor; ?></td>
-                            <td><?= $dtsFiltros->servicio_valor_certificado; ?></td>
-                            <td><?= $dtsFiltros->tipo_servicio_nombre; ?></td>
-                            <td><?= $dtsFiltros->estado; ?></td>
-                            <td style="padding: 0px 0px 0px 0px">
-                                <table align="center" cellpadding="0" cellspacing="0" border="0" >
+                        <table cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Sede</th>
+                                    <th>Servicio</th>
+                                    <th>Vlr Servicio</th>
+                                    <th>Vlr Certificado</th>
+                                    <th>Tipo Servicio</th>
+                                    <th>Estado</th>
+                                    <th>Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                foreach ($filtros as $dtsFiltros) {
+                                    ?>
                                     <tr>
-                                        <td align="center" style="padding: 5px 5px 0px 0px; border-top: #ffffff"><button title="Editar" class="glyphicon glyphicon-edit btn btn-sm btn-info" onclick="enviarPeticionAjax('<?= $urlBase . '/administracion/adminServicio/newServicio/'.$dtsFiltros->servicio_id.'/editServicio'; ?>', 'divTabs');"></button></td>
-                                        <td align="center" style="padding: 5px 5px 0px 0px; border-top: #ffffff"><button title="Consultar" class="glyphicon glyphicon-eye-open btn btn-sm btn-info" onclick="enviarPeticionAjax('<?= $urlBase . '/administracion/adminServicio/newServicio/'.$dtsFiltros->servicio_id.'/consulFilterServi'; ?>', 'divTabs');"></button></td>
+                                        <td><?= $dtsFiltros->sede_nombre; ?></td>
+                                        <td><?= $dtsFiltros->servicio_nombre; ?></td>
+                                        <td><?= $dtsFiltros->servicio_valor; ?></td>
+                                        <td><?= $dtsFiltros->servicio_valor_certificado; ?></td>
+                                        <td><?= $dtsFiltros->tipo_servicio_nombre; ?></td>
+                                        <td><?= $dtsFiltros->estado; ?></td>
+                                        <td style="padding: 0px 0px 0px 0px">
+                                            <table align="center" cellpadding="0" cellspacing="0" border="0" >
+                                                <tr>
+                                                    <td align="center" style="padding: 5px 5px 0px 0px; border-top: #ffffff"><button title="Editar" class="glyphicon glyphicon-edit btn btn-sm btn-info" onclick="enviarPeticionAjax('<?= $urlBase . '/administracion/adminServicio/newServicio/' . $dtsFiltros->servicio_id . '/editServicio'; ?>', 'divTabs');"></button></td>
+                                                    <td align="center" style="padding: 5px 5px 0px 0px; border-top: #ffffff"><button title="Consultar" class="glyphicon glyphicon-eye-open btn btn-sm btn-info" onclick="enviarPeticionAjax('<?= $urlBase . '/administracion/adminServicio/newServicio/' . $dtsFiltros->servicio_id . '/consulFilterServi'; ?>', 'divTabs');"></button></td>
+                                                </tr>
+                                            </table>
+                                        </td>
                                     </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                    ?>                    
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+                                    <?php
+                                }
+                                ?>                    
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
 
+        </td>
+    </tr>
+</table>
 <script type="text/javascript">formatoTablaResultados();</script>

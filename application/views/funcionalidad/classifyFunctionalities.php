bdb18c8baa2efc87d6dc7d0fea1f0598b859804a
arkazero <arkazero@localhost>
2013-12-30 15:26:39 -0500

    modulo funcionalidades

--- application/views/funcionalidad/classifyFunctionalities.php
+++ application/views/funcionalidad/classifyFunctionalities.php
@@ -1,43 +1,48 @@
 <?php $urlBase = base_url('index.php'); ?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link href="<?php echo base_url('css/chosen.min.css'); ?>" rel="stylesheet" media="screen">                               
-    </head>
-    <body>
-        <div id="container">
-            <div>                
-                <div>                    
-                    <div>
-                        Administraci&oacute;n
-                        <?php
-                        $opcFuncionalidad = array();
-                        foreach ($funcionalidades as $func) {
-                            $opcFuncionalidad[$func->funcionalidad_id] = $func->funcionalidad_nombre;
-                        }
-                        $js = 'data-placeholder="Seleccione las Funcionalidades" class="chzn-select input-lg" id="administracion" style="width: 60%; height:20px" multiple="true" tabindex="4"';
-                        echo form_dropdown('administracion', $opcFuncionalidad, '', $js);
-                        ?>                        
-                    </div>
-                    <div>
-                        Administraci&oacute;n
-                        <?php
-                        $opcFuncionalidad = array();
-                        foreach ($funcionalidades as $func) {
-                            $opcFuncionalidad[$func->funcionalidad_id] = $func->funcionalidad_nombre;
-                        }
-                        $js = 'data-placeholder="Seleccione las Funcionalidades" class="chzn-select input-lg" id="administracion" style="width: 60%; height:20px" multiple="true" tabindex="4"';
-                        echo form_dropdown('administracion', $opcFuncionalidad, '', $js);
-                        ?>                        
-                    </div>
-                </div>                               
-                <script type="text/javascript">
-                $(function(){
-                    $(".chzn-select").chosen();
-                });
-                </script>
-            </div>
+<div class="table-responsive">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="text-muted bootstrap-admin-box-title">Listado de funcionalidades</div>
         </div>
-    </body>
-</html>
+        <div class="bootstrap-admin-panel-content">
+
+            <table cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th style="text-align: center">Funcionalidad</th>
+                        <?php
+                        foreach( $grupos as $grupo ){
+                            ?>
+                            <th style="text-align: center"><?php echo $grupo->grupo_nombre;?></th>
+                            <?php
+                        }
+                        ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($funcionalidades as $funcionalidad) {
+                        ?>
+                        <tr class="odd gradeX">
+                            <td><?php echo $funcionalidad->funcionalidad_nombre; ?></td>
+                            <?php
+                            foreach( $grupos as $grupo ){
+                                ?>
+                                <td style="text-align: center">
+                                    <input type="checkbox" name="<?php echo $grupo->grupo_id;?>">
+                                </td>
+                                <?php
+                            }
+                            ?>    
+                        </tr>                            
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<script type="text/javascript">formatoTablaResultados();</script>

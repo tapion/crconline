b0d5a4d89849e8fa904baed1fce5af6d2b15f025
arkazero <arkazero@localhost>
2014-01-08 22:57:51 -0500

    modulo funcionalidades

--- application/views/funcionalidad/classifyFunctionalities.php
+++ application/views/funcionalidad/classifyFunctionalities.php
@@ -5,41 +5,45 @@
             <div class="text-muted bootstrap-admin-box-title">Listado de funcionalidades</div>
         </div>
         <div class="bootstrap-admin-panel-content">
-
-            <table cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th style="text-align: center">Funcionalidad</th>
-                        <?php
-                        foreach( $grupos as $grupo ){
-                            ?>
-                            <th style="text-align: center"><?php echo $grupo->grupo_nombre;?></th>
-                            <?php
-                        }
-                        ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    foreach ($funcionalidades as $funcionalidad) {
-                        ?>
-                        <tr class="odd gradeX">
-                            <td><?php echo $funcionalidad->funcionalidad_nombre; ?></td>
+            <?= form_open('', array('class' => 'form-search', 'id' => 'formOrdenarFuncionalidades')); ?>
+                <table cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th style="text-align: center">Funcionalidad</th>
                             <?php
                             foreach( $grupos as $grupo ){
                                 ?>
-                                <td style="text-align: center">
-                                    <input type="checkbox" name="<?php echo $grupo->grupo_id;?>">
-                                </td>
+                                <th style="text-align: center"><?php echo $grupo->grupo_nombre;?></th>
                                 <?php
                             }
-                            ?>    
-                        </tr>                            
+                            ?>
+                        </tr>
+                    </thead>
+                    <tbody>
                         <?php
-                    }
-                    ?>
-                </tbody>
-            </table>
+                        foreach ($funcionalidades as $funcionalidad) {
+                            ?>
+                            <tr class="odd gradeX">
+                                <td><?php echo $funcionalidad->funcionalidad_nombre; ?></td>
+                                <?php
+                                foreach( $grupos as $grupo ){
+                                    ?>
+                                    <td style="text-align: center">
+                                        <input type="checkbox" name="<?php echo $grupo->grupo_id;?>">
+                                    </td>
+                                    <?php
+                                }
+                                ?>    
+                            </tr>                            
+                            <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <div>
+                    <button class="btn btn-sm btn-danger" type="submit"><strong>Ordenar</strong></button>
+                </div>
+            <?= form_close(); ?>
         </div>
     </div>
 </div>
